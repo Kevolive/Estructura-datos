90d75f92067a5dbd5b7b748b9de886706c19de65
Desarrollo <08ath11@gmail.com>
2024-09-26 20:37:01 -0500

    Se realiza un ejercicio con arreglos y for

--- script.js
+++ script.js
@@ -6,9 +6,23 @@
 // console.log(variasCosas[5])
 // console.log(variasCosas[3])
 // console.log(otroArreglo)
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for (let i = 0; i < numeros.length; i++){
-    // console.log(numeros[i])
-if (numeros[i]  % 2 == 0) { 
-    console.log(numeros[i] * 5)
-}}
+
+// Recorrido de un arreglo
+// let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let i = 0; i < numeros.length; i++){
+//     // console.log(numeros[i])
+// if (numeros[i]  % 2 == 0) { 
+//     console.log(numeros[i] * 5)
+// }
+// }
+
+// Llenado de un arreglo
+// let nombres = []
+
+// for (let i = 0; i < 5; i++){
+//     nombres[i] = prompt("Ingrese un nombre: ")
+    
+// }
+// for(let i = 0; i <nombres.length; i++){
+// console.log(nombres[i])
+// }
