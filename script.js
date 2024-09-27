523297387b95f6d00bf87a550d72e967b25f0d84
Desarrollo <08ath11@gmail.com>
2024-09-26 20:16:47 -0500

    Implemenación de un recorrido a un arreglo de números

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let numero = 31
-let variasCosas = [31, true, 'hola', function() {}, [], numero] /* arreglo definidos */
-let otroArreglo = [] /* Arreglo vacío */
+// let numero = 31
+// let variasCosas = [31, true, 'hola', function() {}, [], numero] /* arreglo definidos */
+// let otroArreglo = [] /* Arreglo vacío */
 
-console.log(variasCosas[0]* variasCosas[5])
-console.log(variasCosas[5])
-console.log(variasCosas[3])
-console.log(otroArreglo)
+// console.log(variasCosas[0]* variasCosas[5])
+// console.log(variasCosas[5])
+// console.log(variasCosas[3])
+// console.log(otroArreglo)
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i = 0; i < numeros.length; i++){
+    // console.log(numeros[i])
+if (numeros[i]  % 2 == 0) { 
+    console.log(numeros[i] * 5)
+}}
